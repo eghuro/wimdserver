27adf64862d522cc8619cc269eed37555d399419
Alexander Mansurov <alexander.mansurov@gmail.com>
2014-10-04 21:06:26 +0200

    UserDeviceDB

--- WIMDserver/src/wimdserver/db/model/UserDeviceDB.java
+++ WIMDserver/src/wimdserver/db/model/UserDeviceDB.java
@@ -6,10 +6,25 @@
 
 package wimdserver.db.model;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Alexander Mansurov <alexander.mansurov@gmail.com>
  */
 public class UserDeviceDB {
     
+    private final HashMap<String,Integer> SID2DID;
+    
+    public UserDeviceDB(){
+        this.SID2DID=new HashMap<>();
+    }
+    
+    public int GetDID(String SID){
+        return SID2DID.get(SID);
+    }
+    
+    public void SetRecord(String SID,int DID){
+        SID2DID.put(SID, DID);
+    }
 }
