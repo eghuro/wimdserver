315664eea250e94eecafc992bb9a3f67e63339cd
Alexander Mansurov <alexander.mansurov@gmail.com>
2014-10-16 09:28:03 +0200

    Parser

--- WIMDserver/src/wimdserver/net/Parser.java
+++ WIMDserver/src/wimdserver/net/Parser.java
@@ -26,61 +26,146 @@ public class Parser {
     final String REC="REC";
     final String DEV="DEV";
     
+    final String SID="SID";
+    final String OTP="OTP";
+    final String DID="DID";
+    final String COORD="COORD";
+    
     final String ERR="ERR";//exception
     final String FAIL="FAIL";//login
     final String OK="OK";
     
     final DatabaseController DC;
     
+    private enum State{
+        START,REC,DEV,
+        SID,SIDV,OTP,OTPV,DID,DIDV,COORD,COORDV,
+        D_UID,D_PWD;
+    }
+    
+    State state;
+    String sid,otp,coord,uid,pwd;
+    int did;
+    
     public Parser(DatabaseController dc){
         this.DC=dc;
+        this.state=State.START;
     }
     public synchronized ParseResult parse(String s){
-         if (s.startsWith(REC)){
-             //CHYBA - dostal 1 slovo!!
-             Pattern p1 = Pattern.compile("REC SID ([:alnum:]+) OTP ([:alnum:]+) DID ([:alnum:]+) COORD (.+)");//TODO
-             Matcher m1 = p1.matcher(s);
-             String sid = m1.group(1);
-             String otp = m1.group(2);
-             String did_s = m1.group(3);
-             int did = Integer.parseInt(did_s);
-             String loc = m1.group(4);
-             try {
-                 DC.newRecord(sid, did, otp, loc);
-             } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
-                 Logger.getLogger(Parser.class.getName()).log(Level.SEVERE, null, ex);
-                 this.result=ERR;
-                 return ParseResult.RESULT;
-             }
-        } else if (s.startsWith(DEV)){
-            //CHYBA - dostal 1 slovo
-            Pattern p2 = Pattern.compile("DEV ([:alnum:]+) ([:alnum:]+)");
-            Matcher m2 = p2.matcher(s);
-            String uid = m2.group(1);
-            String pwd = m2.group(2);
-            
-            try {
-                //pro SID vygeneruje nove DID
-                NewDeviceReturnRecord newDevice = DC.newDevice(uid,pwd);
-                if(newDevice==null){
-                    //auth failed
-                    this.result=FAIL;
-                    return ParseResult.RESULT;
+        switch(state){
+            case START:
+                switch (s) {
+                    case REC:
+                        state=State.REC;
+                        return ParseResult.PARSE;
+                    case DEV:
+                        state=State.DEV;
+                        return ParseResult.PARSE;
+                    default:
+                        state=State.START;
+                        result=FAIL;
+                        return ParseResult.RESULT;
+                }
+            case REC:
+                if(s.equals(SID)){
+                    state=State.SID;
+                    return ParseResult.PARSE;
                 }else{
-                    //auth OK, posle DID, SID a OTP
-                    this.result=OK+" DID "+newDevice.DID+" SID "+newDevice.SID+" OTP "+newDevice.OTP;
+                    state=State.START;
+                    result=FAIL;
                     return ParseResult.RESULT;
                 }
-            } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
-                Logger.getLogger(Parser.class.getName()).log(Level.SEVERE, null, ex);
-                this.result=ERR;
+            case SID:
+                this.sid=s;
+                state=State.SIDV;
+                return ParseResult.PARSE;
+            case SIDV:
+                if(s.equals(OTP)){
+                    state=State.OTP;
+                    return ParseResult.PARSE;
+                }else{
+                    state=State.START;
+                    result=FAIL;
+                    return ParseResult.RESULT;
+                }
+            case OTP:
+                this.otp=s;
+                state=State.OTPV;
+                return ParseResult.PARSE;
+            case OTPV:
+                if(s.equals(DID)){
+                    state=State.DID;
+                }else{
+                    state=State.START;
+                    result=FAIL;
+                    return ParseResult.RESULT;
+                }
+            case DID:
+                try{
+                    this.did=Integer.parseInt(s);
+                    state=State.DIDV;
+                    return ParseResult.PARSE;
+                }catch(NumberFormatException e){
+                    state=State.START;
+                    result=FAIL;
+                    return ParseResult.RESULT;
+                }
+            case DIDV:
+                if(s.equals(COORD)){
+                    state=State.COORDV;
+                    return ParseResult.PARSE;
+                }else{
+                    state=State.START;
+                    result=FAIL;
+                    return ParseResult.RESULT;
+                }
+            case COORDV:
+                this.coord=s;
+                try{
+                    result=DC.newRecord(sid, did, otp, coord);
+                    //CEKEJ
+                    
+                    //POSLI OTP
+                    state=State.START;
+                    if(result==null){
+                        result=FAIL;
+                    }
+                    return ParseResult.RESULT;
+                } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+                    Logger.getLogger(Parser.class.getName()).log(Level.SEVERE, null, ex);
+                    state=State.START;
+                    result=ERR;
+                    return ParseResult.RESULT;
+                }
+            case DEV:
+                state=State.D_UID;
+                return ParseResult.PARSE;
+            case D_UID:
+                state=State.D_PWD;
+                this.uid=s;
+                return ParseResult.PARSE;
+            case D_PWD:
+                try{
+                    NewDeviceReturnRecord ndr = DC.newDevice(uid, pwd);
+                    if(ndr==null){
+                        this.result=FAIL;
+                        state=State.START;
+                        return ParseResult.RESULT;
+                    }else{
+                        result = DID+" "+ndr.DID+" SID "+ndr.SID+" OTP "+ndr.OTP;
+                        state=State.START;
+                        return ParseResult.RESULT;
+                    }
+                }catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+                    state=State.START;
+                    this.result=ERR;
+                    return ParseResult.RESULT;
+                }
+            default:
+                state=State.START;
+                this.result=FAIL;
                 return ParseResult.RESULT;
-            }
-        } else{
-            this.result=ERR;
-            return ParseResult.RESULT;
         }
-        return null;
     }
     public synchronized String getResult(){
         return result;
