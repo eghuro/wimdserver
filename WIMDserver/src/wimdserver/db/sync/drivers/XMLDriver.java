391278234f8ca03498f4b53dfd15d469435364f0
Alexandr Mansurov <alexander.mansurov@gmail.com>
2014-10-24 11:47:30 +0200

    XML Driver - setrow

--- WIMDserver/src/wimdserver/db/sync/drivers/XMLDriver.java
+++ WIMDserver/src/wimdserver/db/sync/drivers/XMLDriver.java
@@ -13,7 +13,6 @@ import java.text.ParseException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -21,7 +20,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import wimdserver.db.sync.model.AuthSalt;
@@ -212,16 +210,33 @@ public class XMLDriver implements IDriver {
                 Element r=(Element)rows.item(i);
                 if(r.getAttribute("primary").equals(key)){
                     t.removeChild(r);
+                    writeback();
                     break;
                 }
             }
-        }
-        writeback();
+        }  
     }
 
     @Override
     public void setRow(String table, Row r) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if((table==null)||(r==null)) return;
+        NodeList tables = DOC.getElementsByTagName("table");
+        Element t = null;
+        boolean fin=false;
+        for(int i=0;i<tables.getLength();i++){
+            t=(Element)tables.item(i);
+            if(t.getAttribute("name").equals(table)){
+                fin=true;
+                break;
+            }
+        }
+        if((fin)&&(t!=null)){  
+            for(String cname:r.getColumnNames()){
+                Element row = DOC.createElement("row");
+                row.setAttribute(cname, r.getItem(cname));
+                t.appendChild(row);
+            }
+        }
     }
 
     @Override
